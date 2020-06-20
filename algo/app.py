63894305a4f1173773b2976aafddffe1cc01a46c
zhangyaowen <zyw19871007@163.com>
2020-06-20 15:47:00 +0800

    common-util

--- algo/app.py
+++ algo/app.py
@@ -1,35 +1,67 @@
-from flask import Flask, escape, request
+# -*- coding: utf8 -*-
+import json
 import sys
+import traceback
+
+from flask import Flask, escape, request, jsonify, Response
+
+from algo import __version__
 
 app = Flask(__name__)
 
+your_rest_server_ip = "0.0.0.0"
+your_rest_server_port = "5000"
+
 
 @app.route('/')
+def index():
+    return 'hello'
+
+
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return f'Hello-1, {escape(name)}!'
 
 
-@app.route('/getQCQty')
-def getQCQty():
-    name = request.args.get("name", "World")
-    # todo 调用算法
-
-    return f'Hello, {escape(name)}!'
+@app.errorhandler(Exception)
+def handle_error(error):
+    print(error, 'error')
+    trace = traceback.format_exc()
+    print(trace)
+    msg = ('%s' % error)
+    return jsonify(code=-1, message='error', data=msg, trace=trace)
 
 
 def run():
-    print(len(sys.argv))
-    if len(sys.argv) == 3:
+    print('version:' + __version__)
+    length = len(sys.argv)
+    if length == 3:
         arg1, arg2 = sys.argv[1], sys.argv[2]
-        print(arg1,arg2)
+        print(arg1, arg2)
         app.run(
             host=arg1,
             port=arg2
         )
+    elif length == 4:
+        arg1, arg2, arg3 = sys.argv[1], sys.argv[2], sys.argv[3]
+        print(arg1, arg2, arg3)
+        app.run(
+            host=arg1,
+            port=arg2,
+            debug=True
+        )
     else:
-        app.run()
+        app.run(
+            host=your_rest_server_ip,
+            port=your_rest_server_port,
+            # debug=True
+        )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(
+        host=your_rest_server_ip,
+        port="12322",
+        debug=True
+    )
--- setup.py
+++ setup.py
@@ -1,24 +1,37 @@
 # -*- coding: utf8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
-from algo import __version__
+from src import __version__
 
 setup(
     name="algo",
     version=__version__,
     author="algo@gmail.com",
-    description="algo.python.tensorflow",
-    packages=["algo"],
+    description="algo.python.hd_tensorflow",
+    packages=find_packages(),
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.json', '*.xml'],
+    },
+    # include_package_data=True,
     install_requires=[
         "flask==1.1.2",
         "tensorflow==2.1.0",
         "numpy==1.18.2",
-        "zc.recipe.egg==2.0.7"
+        "pandas==1.0.3",
+        "scikit-learn==0.22.2",
+        "joblib==0.14.1",
+        "zc.recipe.egg==2.0.7",
+        "zc.zdaemonrecipe==1.0.0",
+        "pyignite==0.3.4",
+        "ortools == 7.6.7691",
+        'py_eureka_client==0.7.4'
+
     ],
     entry_points={
         "console_scripts": [
-            "api=algo.app:run"
+            "api=src.app:run"
         ]
     }
 )
