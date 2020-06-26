0a7eba52be4e0312d6456c7d8f7af67af3134107
zhangyaowen <zyw19871007@163.com>
2020-06-26 15:16:17 +0800

    send email

---
+++ src/common/send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+
+def send_mail(to_email, subject, message, server='smtp.qq.com',
+              from_email='email_address'):
+    # import smtplib
+    print("send email")
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = from_email
+    msg['To'] = ', '.join(to_email)
+
+    msg.attach(message)
+    print('email server')
+    server = smtplib.SMTP_SSL(server, port=465)
+    # server.set_debuglevel(1)
+    server.login(from_email, 'token')  # user & password
+    server.send_message(msg)
+    server.quit()
+    print('successfully sent the mail.')
