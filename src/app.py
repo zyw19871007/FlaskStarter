be8fee9823b99bd0a0c2f119fe892f18fbac5252
zhangyaowen <zyw19871007@163.com>
2020-06-26 16:43:21 +0800

    manager.py

--- src/app.py
+++ src/app.py
@@ -44,7 +44,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    extend_one = db.Column(db.String(80), unique=False)
+    extend_one = db.Column(db.String(81), unique=False)
     extend_two = db.Column(db.String(80), unique=False)
     note = db.Column(db.String(255), unique=False)
 
--- src/manager.py
+++ src/manager.py
@@ -8,6 +8,7 @@ from src.app import app, db
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+# db.create_all()
 
 if __name__ == '__main__':
     manager.run()
